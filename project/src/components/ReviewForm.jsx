03debe712b0f9aeacc7f040ce5829b7fa07cb3a4
Ngunhcho7654 <hakhanh699669@gmail.com>
2025-11-12 16:58:40 +0700

    fix fe

--- project/src/components/ReviewForm.jsx
+++ project/src/components/ReviewForm.jsx
@@ -26,7 +26,7 @@ export default function ReviewForm({ onReviewAdded }) {
     }
 
     try {
-      const res = await fetch('http://localhost:8082/api/reviews', {
+      const res = await fetch('http://localhost:8082/api/public/reviews', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
